106e93444b15c7d71e369529af338684d838bc9b
Lucas Umberger <124316999+lumberger22@users.noreply.github.com>
2024-09-04 13:48:22 -0400

    Update twoSum.py

--- twoSum.py
+++ twoSum.py
@@ -1,1 +1,20 @@
-# Answery Two Sum Problem Here
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
+        hashmap = {}
+        ans = []
+
+        for i, n in enumerate(nums):
+            if target - n in hashmap:
+                ans.append(i)
+                ans.append(hashmap[target - n])
+                return ans
+            else:
+                hashmap[n] = i
+        
+        return ans
